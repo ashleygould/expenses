a32d6453327af6234306b8df7f98fefc9edb9ec3
Ashley Gould <ashleygould@its-agould-9m.local>
2017-10-10 01:00:03 -0700

    add function find_duplicates()
    sort categories when printing

--- expenses.py
+++ expenses.py
@@ -3,11 +3,12 @@
 Reconcile expenses totaled by categories
 
 Usage:
-  expenses -f <csvfile>
+  expenses -f <csvfile> [-v]
   expenses -h
 
 Options:
   -f csvfile  Path to CSV formatted expenses file.
+  -v          Print out expense entries
   -h          Help message
 
 
@@ -29,24 +30,39 @@ def dollars(amount_in_cents):
     return format_currency(amount_in_cents / 100, 'USD')
 
 def report_by_categories(expenses):
-    for category in set([e.Category for e in expenses]):
+    for category in sorted(set([e.Category for e in expenses])):
         amounts = [cents(e.Amount) for e in expenses if e.Category == category]
         if amounts:
             total = reduce(lambda x, y: x+y, amounts)
-            print('{:<16}{:>12}'.format(category + ':', dollars(total)))
+            print('  {:<32}{:>12}'.format(category + ':', dollars(total)))
+
+def find_duplicates(expense_items, duplicate_sets=[]):
+    if len(expense_items) <= 1:
+        return duplicate_sets
+    else:
+        item = expense_items.pop()
+        dups_for_item = [e for e in expense_items
+                if (e.Date, e.Amount, e.Category, e.Property, e.Source, e.CheckNum) ==
+                (item.Date, item.Amount, item.Category, item.Property, item.Source, item.CheckNum)]
+        if dups_for_item:
+            dups_for_item.append(item)
+            duplicate_sets.append(dups_for_item)
+            expense_items = [x for x in expense_items if x not in dups_for_item]
+        return find_duplicates(expense_items, duplicate_sets)
+
 
 def main():
     args = docopt(__doc__)
-    print(args)
     expenses = []
     with open(args['-f'], newline='') as f:
         reader = csv.reader(f)
         header = next(reader)
-        Expense = namedtuple('Expense', [h.strip() for h in header])
+        Expense = namedtuple('Expense', [h.strip() for h in header] + ['line_num'])
         for row in reader:
-            #print(row)
+            if not row:
+                continue
             try:
-                expense = Expense._make(row)
+                expense = Expense._make(row + [reader.line_num])
             except:
                 print('bad row at line %s: %s' % (reader.line_num, row))
                 continue
@@ -57,23 +73,35 @@ def main():
                     print('Category missing at line %s: %s' % (reader.line_num, row))
                 else:
                     expenses.append(expense)
-    #for e in expenses: print(e)
+
+    if args['-v']:
+        for e in expenses: print(e)
 
     by_property = dict()
     for property in set([e.Property for e in expenses]):
         if property:
             by_property[property] = []
         else:
-            by_property['All'] = []
+            by_property['General'] = []
     for e in expenses:
         if not e.Property:
-            by_property['All'].append(e)
+            by_property['General'].append(e)
         else:
             by_property[e.Property].append(e)
+    general = by_property.pop('General')
     for property in sorted(by_property.keys()):
         print("\nProperty: %s" % property)
         report_by_categories(by_property[property])
+    print("\nProperty: General")
+    report_by_categories(general)
 
+    duplicates = find_duplicates(list(expenses))
+    if duplicates:
+        print("\nPosible duplicate expense entries:")
+        for dup_list in duplicates:
+            for item in dup_list:
+                print(item)
+            print()
 
 
 if __name__ == "__main__":
